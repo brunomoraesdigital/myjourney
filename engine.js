8f7f86612d4a8b4fa8d5948dba67bbbf5d5fa257
Bruno Moraes <brunomoraes.dev@gmail.com>
2023-12-03 17:08:41 -0300

    inserção do js e mais alguma responsividade

--- engine.js
+++ engine.js
@@ -8,3 +8,38 @@ const anoAtual = new Date().getFullYear();
 anoAtualElement.textContent = `Copyright ©
 ${anoAtual} Bruno Moraes`;   
 /*********************************/
+
+// Pegar todos os elementos com a classe 'botao'
+var botoes = document.getElementsByClassName('botao');
+
+// Elemento com o ID 'example-element'
+var exemploElemento = document.getElementById('example-element');
+
+/**
+ * Função chamada quando um botão é clicado.
+ * Atualiza a classe do elemento 'example-element' e destaca o botão clicado.
+ */
+function clique() {
+    // Obter o valor específico do botão pressionado
+    var valorBackgroundAttachment = this.innerText.toLowerCase().trim();
+
+    // Atribuir a classe 'selecionado' ao botão clicado
+    for (var i = 0; i < botoes.length; i++) {
+        botoes[i].classList.remove('selecionado');
+    }
+    this.classList.add('selecionado');
+
+    // Remover 'background-attachment:' e ponto e vírgula, e substituir vírgula por espaço
+    exemploElemento.className = valorBackgroundAttachment
+        .replace('background-attachment: ', '')
+        .replace(/;/g, '')
+        .replace(/, /g, '-');
+}
+
+// Iterar sobre cada botão e adicionar um ouvinte de evento de clique
+for (var i = 0; i < botoes.length; i++) {
+    // Adicionar o ouvinte de evento de clique a cada botão
+    // Quando um botão for clicado, a função clique() será chamada
+    botoes[i].addEventListener('click', clique);
+}
+
